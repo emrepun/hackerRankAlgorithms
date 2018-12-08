43026eb4ca8cbf4e43edcf10ab9f610d75caf817
emrepun <emrehavan@hotmail.com>
2018-12-08 20:02:50 +0000

    Library Fine solved

--- src/com/pun/Main.java
+++ src/com/pun/Main.java
@@ -8,7 +8,27 @@ public class Main {
     public static void main(String[] args) {
 
         // Challenge links are given above each function.
+    }
 
+    //https://www.hackerrank.com/challenges/library-fine/problem
+    static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+        if (y1 > y2) {
+            return (y1-y2) * 10000;
+        } else if (y1 == y2) {
+            if (m1 > m2) {
+                return (m1 - m2) * 500;
+            } else if (m1 == m2) {
+                if (d1 > d2) {
+                    return (d1 - d2) * 15;
+                } else {
+                    return 0;
+                }
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
     }
 
     //https://www.hackerrank.com/challenges/sherlock-and-squares/problem
